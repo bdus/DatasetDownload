49333e40a89de8f6cdfc6d7d9493c81263794998
root <root@bdus153.vds>
2019-10-29 15:33:52 +0200

    cloud

--- dataDownload/dataact/restart.sh
+++ dataDownload/dataact/restart.sh
@@ -4,7 +4,7 @@ do
 ps -ef | grep "cloud.py" | grep -v "grep"
 if [ "$?" -eq 1 ]
 then
-./run.sh #启动应用，修改成自己的启动应用脚本或命令
+bash run.sh #启动应用，修改成自己的启动应用脚本或命令
 echo "process has been restarted!"
 else
 echo "process already started!"
