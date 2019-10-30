fde7708ad0ff8c725a3e00a978387349d11d473f
bdus <rovingthrough@163.com>
2019-10-30 23:07:53 +0800

    local

--- dataDownload/dataact/downlib/worker.py
+++ dataDownload/dataact/downlib/worker.py
@@ -121,10 +121,22 @@ class TheParty(object):
 #        try:
 #            uplist = os.listdir(self.LDlDir) ##a,b = os.path.splitext()
 #            ans = self.bp.syncup(self.LDlDir,self.RDir)
-#            resp = self.chkok(ans)
-#            if resp:                
-#                self._addID(youtube_id,self.uploaded_video)
-##        
+#            resp = self._chkok(ans)
+#            for item in uplist:
+#                file,_ = os.path.splitext(item)
+#                if not resp:
+#                    ans = self.upload(item)
+#                    assert self._chkok(ans)
+#                os.remove(fpath)
+#                self.dlqueue.remove(item)
+#                print str(item),' uploaded. deleted'
+#                self._addID(item,self.uploaded_video)
+#                
+#        except Exception,e:
+#            print 'upload failed.'
+#            print self.bp.response.json()
+#            print e
+
             
     def process(self,youtube_id):
         if len(self.dlqueue) < self.que_max: #如果队列够少，就继续下载;如果队列足够self.que_max，就开始上传
@@ -147,74 +159,74 @@ class TheParty(object):
                     continue
 
     
-def download(youtube_id):    
-    url_base='https://www.youtube.com/watch?v='
-    path = os.path.join('tmp' ,''.join([youtube_id,".mp4"]) )
-    download_url = '%s' % (url_base + youtube_id)
-    ydl_opts = {
-        # outtmpl 格式化下载后的文件名，避免默认文件名太长无法保存 http://www.yujzw.com/python/python-youtube-dl.html
-        'format' : 'best', 
-        'quiet' : True,
-        'outtmpl': u'tmp/%(id)s.%(ext)s'
-    }
-    try:
-        ydl = youtube_dl.YoutubeDL(ydl_opts)
-        print 'downloading ',youtube_id
-        ydl.download([download_url])
-    except youtube_dl.utils.DownloadError,err:
-        print 'ydl error! Add to bad_video list.'
-        for arg in err.args:
-            print arg
-        fo = open("bad_video.log","a")
-        fo.write(youtube_id)
-        fo.write("\n")
-        fo.close()   
-
-
-def chkok(by, result):
-    ans = True
-    if by.processes == 1:
-        if result != const.ENoError and result != const.EHashMismatch:
-            print "Failed, result: {}".format(result)
-            print bp.response.json()
-            ans = False
-    else:
-        if result != const.ENoError and result != const.IEFileAlreadyExists and result != const.EHashMismatch:
-            print "Failed, result: {}".format(result)
-            print bp.response.json()
-            ans = False
-    return ans
-
-def upload_job(youtube_id,path_bdnet):
-    path = os.path.join('tmp' ,''.join([youtube_id,".mp4"]) )
-    bp =  ByPy()
-    try:
-        print 'download ok. uploading.'
-        ans = bp.upload(localpath=path, remotepath=path_bdnet, ondup=u'overwrite')
-        resp = chkok(bp,ans)
-        print 'ans:'+str(ans)+';'
-        if os.path.exists(path):
-            with open('bdnet.txt',"a") as fo:
-                fo.write(youtube_id)
-                fo.write("\n")
-                fo.close()
-    except Exception,e:
-        print 'upload failed.'
-        print bp.response.json()
-        print e        
-
-def del_job(youtube_id):
-    path = os.path.join('tmp' ,''.join([youtube_id,".mp4"]) )    
-    if os.path.exists(path):        
-        os.remove(path)
-        print 'file exist. deleted'
-
-def workjob(youtubeid,path):
-    download(youtubeid)
-    tmppath = os.path.join('tmp' ,''.join([youtubeid,".mp4"]) )
-    if os.path.exists(tmppath) == True:
-        upload_job(youtubeid,path)
-        del_job(youtubeid)
+#def download(youtube_id):    
+#    url_base='https://www.youtube.com/watch?v='
+#    path = os.path.join('tmp' ,''.join([youtube_id,".mp4"]) )
+#    download_url = '%s' % (url_base + youtube_id)
+#    ydl_opts = {
+#        # outtmpl 格式化下载后的文件名，避免默认文件名太长无法保存 http://www.yujzw.com/python/python-youtube-dl.html
+#        'format' : 'best', 
+#        'quiet' : True,
+#        'outtmpl': u'tmp/%(id)s.%(ext)s'
+#    }
+#    try:
+#        ydl = youtube_dl.YoutubeDL(ydl_opts)
+#        print 'downloading ',youtube_id
+#        ydl.download([download_url])
+#    except youtube_dl.utils.DownloadError,err:
+#        print 'ydl error! Add to bad_video list.'
+#        for arg in err.args:
+#            print arg
+#        fo = open("bad_video.log","a")
+#        fo.write(youtube_id)
+#        fo.write("\n")
+#        fo.close()   
+#
+#
+#def chkok(by, result):
+#    ans = True
+#    if by.processes == 1:
+#        if result != const.ENoError and result != const.EHashMismatch:
+#            print "Failed, result: {}".format(result)
+#            print bp.response.json()
+#            ans = False
+#    else:
+#        if result != const.ENoError and result != const.IEFileAlreadyExists and result != const.EHashMismatch:
+#            print "Failed, result: {}".format(result)
+#            print bp.response.json()
+#            ans = False
+#    return ans
+#
+#def upload_job(youtube_id,path_bdnet):
+#    path = os.path.join('tmp' ,''.join([youtube_id,".mp4"]) )
+#    bp =  ByPy()
+#    try:
+#        print 'download ok. uploading.'
+#        ans = bp.upload(localpath=path, remotepath=path_bdnet, ondup=u'overwrite')
+#        resp = chkok(bp,ans)
+#        print 'ans:'+str(ans)+';'
+#        if os.path.exists(path):
+#            with open('bdnet.txt',"a") as fo:
+#                fo.write(youtube_id)
+#                fo.write("\n")
+#                fo.close()
+#    except Exception,e:
+#        print 'upload failed.'
+#        print bp.response.json()
+#        print e        
+#
+#def del_job(youtube_id):
+#    path = os.path.join('tmp' ,''.join([youtube_id,".mp4"]) )    
+#    if os.path.exists(path):        
+#        os.remove(path)
+#        print 'file exist. deleted'
+#
+#def workjob(youtubeid,path):
+#    download(youtubeid)
+#    tmppath = os.path.join('tmp' ,''.join([youtubeid,".mp4"]) )
+#    if os.path.exists(tmppath) == True:
+#        upload_job(youtubeid,path)
+#        del_job(youtubeid)
 
 if __name__ == '__main__':
     path = 'tmp'
