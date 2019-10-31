7cf4a625e5ba5029fe5286f7c5faf5ee3ae4e205
bdus <rovingthrough@163.com>
2019-10-31 16:23:59 +0800

    final

--- dataDownload/ActivityNet/cloud.py
+++ dataDownload/ActivityNet/cloud.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Created on Fri Oct 18 17:30:54 2019
@@ -9,60 +9,63 @@ Created on Fri Oct 18 17:30:54 2019
 import argparse
 import os
 
-from JsonLoader import urlkeyIter
-from worker import workjob
+from lib.JsonLoader import readJsonSet
+from lib.readCSV import readCSVSet
+from lib.readTXT import readTXTSet
+from lib.worker import workjob
+
 '''
 conda activate videodl
 
-python cloud.py ActivityNet-200/activity_net.v1-3.min.json act200
-python cloud.py ActivityNet-100/activity_net.v1-3.min.json act100
+python cloud.py data/ActivityNet-200/activity_net.v1-3.min.json json act200
+python cloud.py data/ActivityNet-100/activity_net.v1-2.min.json json act100
 '''
-def readSet(filename):
-    file = open(filename,'r')
-    badlist = file.readlines()
-    badlist = [i.rstrip('\n') for i in badlist]
-    bad = set(badlist)
-    return bad
 
-def theIter(input_json): 
+def readSet(input_file,fmt):
+    if fmt == 'json':
+        dataset = readJsonSet(input_file)
+    elif fmt == 'txt':
+        dataset = readTXTSet(input_file)
+    elif fmt == 'csv':
+        dataset = readCSVSet(input_file)
+    else:
+        return {}
+    return dataset
+
+def setOp(dataset): 
     #bad set
-    badfile = 'bad_video.log'
-    badset = readSet(badfile)   
-    #downloaded
-    dedfile1 = 'act100.txt'
-    dedset1 = readSet(dedfile1)
-    dedfile2 = 'act200.txt'
-    dedset2 = readSet(dedfile2)
-    dedfile3 = 'bdnet.txt'
-    dedset3 = readSet(dedfile3)
-    
-    #video list    
-    for item in urlkeyIter(input_json):
-        if item in badset:
-            continue
-        if item in dedset1:
-            continue
-        if item in dedset2:
-            continue
-        if item in dedset3:
-            continue
-        yield item
+    #badfiles = ['bad_video.log','act100.txt','bdnet.txt']
+    badfiles = ['bad_video.log','act100.txt','bdnet.txt']
+    badset = set()
+    for badfile in badfiles:
+        if os.path.exists(badfile) == True:
+            tmpset = readTXTSet(badfile)
+            badset = set.union(badset,set(tmpset))
 
-def main(input_json, output_dir,num_jobs):
-    # print(input_json)
-    # print(output_dir)
-    # print(num_jobs)    
-    cnt = 0
-    for i in theIter(input_json):    
-        workjob(i,output_dir)
+    #video list    
+    return set.difference(set(dataset),badset)
+    
+
+def main(args):
+#    print args.input_file
+#    print args.output_dir
+#    print args.fmt
+#    print(args.input_file)
+#    print( args.output_dir)
+#    print( args.fmt)
+    dataset = readSet(args.input_file,args.fmt)
+    for i in setOp(dataset):
+        #workjob(i,output_dir)
+        print(i)
     
 
 if __name__ == '__main__':
     description = 'Helper script for downloading and trimming kinetics videos.'
     p = argparse.ArgumentParser(description=description)
-    p.add_argument('input_json',type=str, help=('ActivityNet Json file') ) 
+    p.add_argument('input_file',type=str, help=('input file name')) 
+    p.add_argument('fmt',type=str,default='json',choices=['json','txt','csv'],help=('Input file format') ) 
     p.add_argument('output_dir',type=str, help=('Output directory where videos will be saved.') ) 
-    p.add_argument('-n', '--num-jobs', type=int, default=2)
-
-    main(**vars(p.parse_args() ) )
+    #p.add_argument('-n', '--num-jobs', type=int, default=2)
+    #main(**vars(p.parse_args() ) )
+    main( p.parse_args() )
 	
